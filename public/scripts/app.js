fffb9ba178ddbc8758f917a407e5c8e99bb698da
Chantal <chandesrochers@gmail.com>
2018-05-22 23:56:34 +0000

    replicated twitter card with issues

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,44 @@
-function createTweetElement(twObj) {
-  var handle = twObj["user"]["handle"];
-  console.log(handle);
-  var $tweet = $('p').val(handle).parents('article');
-  return $tweet;
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
 }
+
+
+
+function createTweetElement(twObj) {
+var $tweet = $("<article>").addClass("all-tweets");
+ var $header = $("<header>").appendTo($tweet);
+ var $footer = $("<footer>").appendTo($tweet);
+ $("<img>").attr("src", twObj.user.avatars.small).appendTo($header);
+ $("<h2>").text(twObj.user.name).appendTo($header);
+ $("<p>").text(twObj.user.handle).appendTo($header);
+ $("<p>").addClass("tweet-content").text(twObj.content.text).appendTo($tweet);
+ $("<p>").text(twObj.created_at).appendTo($footer);
+  var $icons = $("<div>").addClass("tweet-icons").appendTo($footer);
+$("<i>").addClass("fas fa-flag").appendTo($icons);
+$("<i>").addClass("fas fa-retweet").appendTo($icons);
+$("<i>").addClass("fas fa-heart").appendTo($icons);
+
+ return $tweet;
+
+}
+
+$(document).ready(function() {
+    var $tweet = createTweetElement(tweetData);
+    $('.all-tweets').append($tweet)
+});
+
 
 
 
