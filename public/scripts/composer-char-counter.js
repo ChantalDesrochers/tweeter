1cb95c866ac201fd6959b7ac1548a46bb2dc35ba
Chantal <chandesrochers@gmail.com>
2018-05-22 17:53:27 +0000

    refactored code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   $('.new-tweet').find('textarea').on('keyup', function() {
-  var charCount = $(this).val().length;
-  // var counter = $(this).find('.counter').text();
-  // var counter2 = $(this).children('span.counter').text(); // eventually get more specific
-  var inremain = 140 - charCount;
-  if (inremain < 0) {
-    $('.counter').text(inremain).css("color", "red");
-  } else {
-    $('.counter').text(inremain);
-  }
+    const totalChar = 140;
+    const charCount = $(this).val().length;
+    const inremain = totalChar - charCount;
+    if (inremain < 0) {
+      $(this).siblings('.counter').text(inremain).css("color", "red");
+    } else if (inremain >= 0) {
+      $(this).siblings('.counter').text(inremain).css("color", "black");
+    }
+  });
 });
-});
-
