8dc26febbd013c64ff5911117e79e9e1e8f9c1a5
Chantal <chandesrochers@gmail.com>
2018-05-23 00:41:58 +0000

    using json data to render tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,26 +17,78 @@ const tweetData = {
 
 
 function createTweetElement(twObj) {
-var $tweet = $("<article>").addClass("all-tweets");
- var $header = $("<header>").appendTo($tweet);
- var $footer = $("<footer>").appendTo($tweet);
- $("<img>").attr("src", twObj.user.avatars.small).appendTo($header);
- $("<h2>").text(twObj.user.name).appendTo($header);
- $("<p>").text(twObj.user.handle).appendTo($header);
- $("<p>").addClass("tweet-content").text(twObj.content.text).appendTo($tweet);
- $("<p>").text(twObj.created_at).appendTo($footer);
-  var $icons = $("<div>").addClass("tweet-icons").appendTo($footer);
-$("<i>").addClass("fas fa-flag").appendTo($icons);
-$("<i>").addClass("fas fa-retweet").appendTo($icons);
-$("<i>").addClass("fas fa-heart").appendTo($icons);
 
- return $tweet;
+
+
+// var $tweet = $("<article>").addClass("all-tweets");
+//  var $header = $("<header>").appendTo($tweet);
+
+//  $("<img>").attr("src", twObj.user.avatars.small).appendTo($header);
+//  $("<h2>").text(twObj.user.name).appendTo($header);
+//  $("<p>").text(twObj.user.handle).appendTo($header);
+//  $("<p>").addClass("tweet-content").text(twObj.content.text).appendTo($tweet);
+//  var $footer = $("<footer>").appendTo($tweet);
+//  $("<p>").text(twObj.created_at).appendTo($footer);
+//   var $icons = $("<div>").addClass("tweet-icons").appendTo($footer);
+// $("<i>").addClass("fas fa-flag").appendTo($icons);
+// $("<i>").addClass("fas fa-retweet").appendTo($icons);
+// $("<i>").addClass("fas fa-heart").appendTo($icons);
+
+//  return $tweet;
+
+// }
+
+
+var $tweet = $("<article>").addClass("all-tweets");
+var aTweet =  `
+
+        <header>
+          <img src="
+https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png">
+          <h2>Bill Fields</h2>
+          <p>@MrFields</p>
+        </header>
+        <p class="tweet-content">Little tweet here</p>
+        <footer><p>10 days ago</p>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      `
+
+// $tweet.append(aTweet);
+
+var formattedDate = new Date(twObj.created_at);
+
+$tweet.append(`
+          <header>
+          <img src="${twObj.user.avatars.small}"/>
+          <h2>${twObj.user.name}</h2>
+          <p>${twObj.user.handle}</p>
+        </header>
+        <p class="tweet-content">${twObj.content.text}</p>
+        <footer><p>${formattedDate.toDateString()}</p>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>`);
+
+return $tweet;
 
 }
 
+
+
+
+
+
 $(document).ready(function() {
     var $tweet = createTweetElement(tweetData);
-    $('.all-tweets').append($tweet)
+    $('#allTweets').append($tweet)
 });
 
 
