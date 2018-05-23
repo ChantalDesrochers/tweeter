9434403c10c7bc3bc22802932605e7297964656d
Chantal <chandesrochers@gmail.com>
2018-05-23 16:46:16 +0000

    rendering tweets from array

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,73 @@
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
 
 
 
 function createTweetElement(twObj) {
 
-
-
-// var $tweet = $("<article>").addClass("all-tweets");
-//  var $header = $("<header>").appendTo($tweet);
-
-//  $("<img>").attr("src", twObj.user.avatars.small).appendTo($header);
-//  $("<h2>").text(twObj.user.name).appendTo($header);
-//  $("<p>").text(twObj.user.handle).appendTo($header);
-//  $("<p>").addClass("tweet-content").text(twObj.content.text).appendTo($tweet);
-//  var $footer = $("<footer>").appendTo($tweet);
-//  $("<p>").text(twObj.created_at).appendTo($footer);
-//   var $icons = $("<div>").addClass("tweet-icons").appendTo($footer);
-// $("<i>").addClass("fas fa-flag").appendTo($icons);
-// $("<i>").addClass("fas fa-retweet").appendTo($icons);
-// $("<i>").addClass("fas fa-heart").appendTo($icons);
-
-//  return $tweet;
-
-// }
-
-
-var $tweet = $("<article>").addClass("all-tweets");
-var aTweet =  `
-
-        <header>
-          <img src="
-https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png">
-          <h2>Bill Fields</h2>
-          <p>@MrFields</p>
-        </header>
-        <p class="tweet-content">Little tweet here</p>
-        <footer><p>10 days ago</p>
-          <div class="tweet-icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </div>
-        </footer>
-      `
-
-// $tweet.append(aTweet);
+var $tweet = $("<article>").addClass("all-tweets"); //rename classes and ids
 
 var formattedDate = new Date(twObj.created_at);
 
@@ -81,15 +90,18 @@ return $tweet;
 
 }
 
-
-
-
-
+function renderTweets(tweets) {
+ tweets.forEach(function(tweet) {
+   createTweetElement(tweet).appendTo("#tweets-container");
+ });
+}
 
 $(document).ready(function() {
-    var $tweet = createTweetElement(tweetData);
-    $('#allTweets').append($tweet)
+ renderTweets(data);
 });
+
+
+
 
 
 
